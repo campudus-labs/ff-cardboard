cffd005aaee30b09148099ea3c3186ba62bcc254
Max Stemplinger <ms@campudus.com>
2015-06-21 19:15:54 +0200

    campudus office 3d, bike model, model loader, threejs update

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,10 +2,14 @@ var THREE = require('threejs');
 require('./StereoEffect');
 require('./DeviceOrientationControls');
 require('./OrbitControls');
+require('./MTLLoader');
+require('./DDSLoader');
+require('./OBJMTLLoader');
 var keepAwake = require('./keepAwake');
 var camera, scene, renderer;
 var effect, controls;
 var element, container;
+var sphere, bike;
 
 var clock = new THREE.Clock();
 
@@ -23,7 +27,7 @@ function init() {
   scene = new THREE.Scene();
 
   camera = new THREE.PerspectiveCamera(90, 1, 0.001, 700);
-  camera.position.set(0, 10, 0);
+  camera.position.set(0, 0, 0);
   scene.add(camera);
 
   controls = new THREE.OrbitControls(camera, element);
@@ -53,30 +57,44 @@ function init() {
   window.addEventListener('deviceorientation', setOrientationControls, true);
 
 
-  var light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);
-  scene.add(light);
+  //var light = new THREE.HemisphereLight(0x777777, 0x111111, 0.4);
+  //scene.add(light);
 
-  var texture = THREE.ImageUtils.loadTexture(
-    'textures/patterns/checker.png'
-  );
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat = new THREE.Vector2(50, 50);
-  texture.anisotropy = renderer.getMaxAnisotropy();
+  var ambient = new THREE.AmbientLight( 0x888888 );
+  scene.add( ambient );
 
-  var material = new THREE.MeshPhongMaterial({
-    color : 0xffffff,
-    specular : 0xffffff,
-    shininess : 20,
-    shading : THREE.FlatShading,
-    map : texture
-  });
+  //var material = new THREE.MeshPhongMaterial({
+  //  color: 0xffffff,
+  //  specular: 0xffffff,
+  //  shininess: 20,
+  //  shading: THREE.FlatShading,
+  //  map: texture
+  //});
 
-  var geometry = new THREE.PlaneGeometry(1000, 1000);
 
-  var mesh = new THREE.Mesh(geometry, material);
-  mesh.rotation.x = -Math.PI / 2;
-  scene.add(mesh);
+  // creation of a big sphere geometry
+  var sphere = new THREE.SphereGeometry(100, 100, 40);
+  sphere.applyMatrix(new THREE.Matrix4().makeScale(-1, 0.75, 1));
+
+  // creation of the sphere material
+  var sphereMaterial = new THREE.MeshBasicMaterial();
+  sphereMaterial.map = THREE.ImageUtils.loadTexture('textures/campudus.jpg');
+
+  // geometry + material = mesh (actual object)
+  var sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
+  scene.add(sphereMesh);
+
+  //Load object model
+  //THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+  //var loader = new THREE.OBJMTLLoader();
+  //var objURL = 'models/human/muro.obj';
+  //var objMTL = 'models/human/muro.mtl';
+  //loader.load( objURL, objMTL, function ( object ) {
+  //  object.scale.set(0.1,0.1,0.1);
+  //  object.position.z = 10;
+  //  bike = object;
+  //  scene.add( object );
+  //} );
 
   window.addEventListener('resize', resize, false);
   setTimeout(resize, 1);
@@ -93,8 +111,13 @@ function resize() {
   effect.setSize(width, height);
 }
 
+var i = 0;
+
 function update(dt) {
   resize();
+  //i = ((i+0.01)%360);
+  //sphere.position.z = Math.sin(i* (Math.PI/180))*20;
+  //sphere.position.x = Math.cos(i* (Math.PI/180))*20;
 
   camera.updateProjectionMatrix();
 
