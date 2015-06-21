83c62072365079de36ebda7c3969e924b5fd9a35
Joern Bernhardt <jb@campudus.com>
2015-06-21 16:39:26 +0200

    Use data-url and webm video to keep awake, remove manifest
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/main/js/keepAwake.js
+++ src/main/js/keepAwake.js
@@ -42,10 +42,10 @@ function stop() {
 function createVideoElement() {
   var video = document.createElement('video');
   video.id = 'keep-awake-video';
-  video.src = 'empty.avi';
-  video.autoPlay = true;
+  video.src = "data:video/webm;base64,GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA=";
   video.loop = true;
   document.body.appendChild(video);
+  video.play();
 }
 
 module.exports = {
