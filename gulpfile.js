dcc9884f4adc3491ee5b2424698f3c8afa47c9a4
Max Stemplinger <ms@campudus.com>
2015-06-22 20:31:30 +0200

    splitted code in different examples

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,12 @@ var reload = browserSync.reload;
 var plumber = require('gulp-plumber');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var foreach = require('gulp-foreach');
+
+var examples = [
+  // "bike_rotating",
+  "campudus_office_360"
+];
 
 gulp.task('sass', sassCompile);
 gulp.task('assets', assetCopy);
@@ -23,49 +29,57 @@ gulp.task('default', ['build']);
 
 
 function sassCompile() {
-  return gulp.src('src/main/scss/main.scss')
-    .pipe(plumber({
-      errorHandler : function (error) {
-        console.log(error.message);
-        this.emit('end');
-      }
+  return gulp.src('src/main/examples/*')
+    .pipe(foreach(function (stream, file) {
+      var exampleName = file.path.replace(/.*src\/main\/examples\//, '');
+      var outPath = 'out/examples/' + exampleName + '/css';
+      return gulp.src(file.path + '/scss/style.scss')
+        .pipe(plumber({
+          errorHandler : function (error) {
+            console.log(error.message);
+            this.emit('end');
+          }
+        }))
+        .pipe(compass({
+          project : Path.join(__dirname),
+          css : outPath,
+          sass : 'src/main/examples/' + exampleName + '/scss',
+          image : 'src/main/examples/' + exampleName + '/img'
+        }))
+        .pipe(minifyCss())
+        .pipe(gulp.dest(outPath));
     }))
-    .pipe(compass({
-      project : Path.join(__dirname),
-      css : 'out/css',
-      sass : 'src/main/scss',
-      image : 'src/main/img'
-    }))
-    .pipe(minifyCss())
-    .pipe(gulp.dest('out/css'));
 }
 
 function scriptCompile() {
-  return browserify()
-    .add('./src/main/js/app.js')
-    .bundle()
-    .on('error', function (err) {
-      console.log('error', err);
-      this.emit('end');
-    })
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('out/js/'));
+  return gulp.src('src/main/examples/*')
+    .pipe(foreach(function (stream, file) {
+      return browserify()
+        .add(file.path + '/js/app.js')
+        .bundle()
+        .on('error', function (err) {
+          console.log('error', err);
+          this.emit('end');
+        })
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('out/examples/' + file.path.replace(/.*src\/main\/examples/, '') + '/js'));
+    }))
 }
 
 function assetCopy() {
-  return gulp.src(['src/main/**', '!src/main/js/**', '!src/main/scss', '!src/main/scss/**'])
+  return gulp.src(['src/main/**', '!src/main/examples/**/js/**', '!src/main/**/scss', '!src/main/**/scss/**'])
     .pipe(gulp.dest('out/'));
 }
 
 function server() {
   browserSync({
-    open: false,
+    open : false,
     server : {
       baseDir : 'out'
     }
   });
 
-  gulp.watch(['src/main/**', 'src/main/js/**', 'src/main/scss/**/*.scss'], {}, ['reloader']);
+  gulp.watch(['src/main/**', 'src/main/**/js/**', 'src/main/**/scss/**/*.scss'], {}, ['reloader']);
 
 }
 
--- src/main/examples/bike_rotating/js/app.js
+++ src/main/examples/bike_rotating/js/app.js
@@ -1,15 +1,13 @@
 var THREE = require('threejs');
-require('./StereoEffect');
-require('./DeviceOrientationControls');
-require('./OrbitControls');
-require('./MTLLoader');
-require('./DDSLoader');
-require('./OBJMTLLoader');
-var keepAwake = require('./keepAwake');
+require('../../../../vendor/threejs/StereoEffect');
+require('../../../../vendor/threejs/DeviceOrientationControls');
+require('../../../../vendor/threejs/OrbitControls');
+require('../../../../vendor/threejs/OBJLoader');
+var keepAwake = require('../../../js/keepAwake');
 var camera, scene, renderer;
 var effect, controls;
 var element, container;
-var sphere, bike;
+var bike;
 
 var clock = new THREE.Clock();
 
@@ -27,7 +25,7 @@ function init() {
   scene = new THREE.Scene();
 
   camera = new THREE.PerspectiveCamera(90, 1, 0.001, 700);
-  camera.position.set(0, 0, 0);
+  camera.position.set(0, 10, 0);
   scene.add(camera);
 
   controls = new THREE.OrbitControls(camera, element);
@@ -57,44 +55,42 @@ function init() {
   window.addEventListener('deviceorientation', setOrientationControls, true);
 
 
-  //var light = new THREE.HemisphereLight(0x777777, 0x111111, 0.4);
-  //scene.add(light);
+  var light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);
+  scene.add(light);
 
-  var ambient = new THREE.AmbientLight( 0x888888 );
-  scene.add( ambient );
+  var ambient = new THREE.AmbientLight(0x111111);
+  scene.add(ambient);
 
-  //var material = new THREE.MeshPhongMaterial({
-  //  color: 0xffffff,
-  //  specular: 0xffffff,
-  //  shininess: 20,
-  //  shading: THREE.FlatShading,
-  //  map: texture
-  //});
+  var texture = THREE.ImageUtils.loadTexture(
+    'textures/patterns/checker.png'
+  );
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat = new THREE.Vector2(50, 50);
+  texture.anisotropy = renderer.getMaxAnisotropy();
 
+  var material = new THREE.MeshPhongMaterial({
+    color : 0xffffff,
+    specular : 0xffffff,
+    shininess : 20,
+    shading : THREE.FlatShading,
+    map : texture
+  });
 
-  // creation of a big sphere geometry
-  var sphere = new THREE.SphereGeometry(100, 100, 40);
-  sphere.applyMatrix(new THREE.Matrix4().makeScale(-1, 0.75, 1));
-
-  // creation of the sphere material
-  var sphereMaterial = new THREE.MeshBasicMaterial();
-  sphereMaterial.map = THREE.ImageUtils.loadTexture('textures/campudus.jpg');
-
-  // geometry + material = mesh (actual object)
-  var sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
-  scene.add(sphereMesh);
+  var geometry = new THREE.PlaneGeometry(1000, 1000);
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.rotation.x = -Math.PI / 2;
+  scene.add(mesh);
 
   //Load object model
-  //THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
-  //var loader = new THREE.OBJMTLLoader();
-  //var objURL = 'models/human/muro.obj';
-  //var objMTL = 'models/human/muro.mtl';
-  //loader.load( objURL, objMTL, function ( object ) {
-  //  object.scale.set(0.1,0.1,0.1);
-  //  object.position.z = 10;
-  //  bike = object;
-  //  scene.add( object );
-  //} );
+  var loader = new THREE.OBJLoader();
+  var objURL = 'models/bike/bike.obj';
+  loader.load(objURL, function (object) {
+    object.scale.set(0.1, 0.1, 0.1);
+    object.position.z = 10;
+    bike = object;
+    scene.add(object);
+  });
 
   window.addEventListener('resize', resize, false);
   setTimeout(resize, 1);
@@ -115,9 +111,10 @@ var i = 0;
 
 function update(dt) {
   resize();
-  //i = ((i+0.01)%360);
-  //sphere.position.z = Math.sin(i* (Math.PI/180))*20;
-  //sphere.position.x = Math.cos(i* (Math.PI/180))*20;
+  if (bike) {
+    i = ((i + 0.5) % 360);
+    bike.rotation.y = i * (Math.PI / 180);
+  }
 
   camera.updateProjectionMatrix();
 
